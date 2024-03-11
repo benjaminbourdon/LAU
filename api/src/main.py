5773d02bd2c3b7d3d9b69fb7b51c2cf28254fd8b
Benjamin B <dev@benjamin-b.fr>
2024-03-11 12:59:03 +0100

    feat(api): Add a CORS Middleware to control origins allowed
    Needed to add frontend as an allowed origins by api CORS rules

--- api/src/main.py
+++ api/src/main.py
@@ -1,13 +1,26 @@
+import os
 from uuid import uuid4
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import settings
 from .dependencies.db import CollectionDep, SessionDep
 from .models.videos import VideoDB, VideoIn, VideoOut
 
+load_dotenv()
+API_ALLOW_ORIGINS = os.getenv("API_ALLOW_ORIGINS", default=())
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=API_ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -15,7 +28,7 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/create", response_model_exclude_unset=True)
+@app.post("/video", response_model_exclude_unset=True)
 async def create_video(
     collection: CollectionDep, session: SessionDep, new_video: VideoIn
 ) -> VideoOut:
